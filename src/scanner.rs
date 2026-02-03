bc4cca3d624da3c6acaaf6d197f95630ba7314e1
blackfrog638 <blackfrog638@gmail.com>
2026-02-04 00:59:13 +0800

    feat: peek character

--- src/scanner.rs
+++ src/scanner.rs
@@ -46,25 +46,115 @@ impl Scanner {
             ')' => self.add_token(TokenType::RightParen, None),
             '{' => self.add_token(TokenType::LeftBrace, None),
             '}' => self.add_token(TokenType::RightBrace, None),
-            ',' => self.add_token(TokenType::COMMA, None),
-            '.' => self.add_token(TokenType::DOT, None),
-            '-' => self.add_token(TokenType::MINUS, None),
-            '+' => self.add_token(TokenType::PLUS, None),
-            ';' => self.add_token(TokenType::SEMICOLON, None),
-            '*' => self.add_token(TokenType::STAR, None),
+            ',' => self.add_token(TokenType::Comma, None),
+            '.' => self.add_token(TokenType::Dot, None),
+            '-' => self.add_token(TokenType::Minus, None),
+            '+' => self.add_token(TokenType::Plus, None),
+            ';' => self.add_token(TokenType::Semicolon, None),
+            '*' => self.add_token(TokenType::Star, None),
+            '!' => {
+                if self.match_char('=') {
+                    self.add_token(TokenType::BangEqual, None)
+                } else {
+                    self.add_token(TokenType::Bang, None)
+                }
+            }
+            '=' => {
+                if self.match_char('=') {
+                    self.add_token(TokenType::EqualEqual, None)
+                } else {
+                    self.add_token(TokenType::Equal, None)
+                }
+            }
+            '>' => {
+                if self.match_char('=') {
+                    self.add_token(TokenType::GreaterEqual, None)
+                } else {
+                    self.add_token(TokenType::Greater, None)
+                }
+            }
+            '<' => {
+                if self.match_char('=') {
+                    self.add_token(TokenType::LessEqual, None)
+                } else {
+                    self.add_token(TokenType::Less, None)
+                }
+            }
+            '/' => {
+                if self.match_char('/') {
+                    // A comment goes until the end of the line.
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash, None);
+                }
+            }
+            ' ' | '\r' | '\t' => {
+                // Ignore whitespace.
+            }
+            '\n' => {
+                self.line += 1;
+            }
+            '\"' => self.string(),
+            '0'..='9' => {
+                // !TODO: Number literal handling would go here.
+            }
             _ => {
                 error(self.line, "Unexpected character");
             }
         }
     }
+
     fn advance(&mut self) -> char {
         let c = self.source[self.current];
         self.current += 1;
         c
     }
+
     fn add_token(&mut self, token_type: TokenType, literal: Option<Literal>) {
         let text: String = self.source[self.start..self.current].iter().collect();
         let token = Token::new(token_type, text, literal, self.line);
         self.tokens.push(token);
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source[self.current]
+        }
+    }
+
+    fn match_char(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.source[self.current] != expected {
+            return false;
+        }
+        self.current += 1;
+        true
+    }
+
+    fn string(&mut self) {
+        loop {
+            if self.peek() == '\"' || self.is_at_end() {
+                break;
+            }
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+        if self.is_at_end() {
+            error(self.line, "Unterminated string.");
+            return;
+        }
+        self.advance();
+        let value: String = self.source[self.start + 1..self.current - 1]
+            .iter()
+            .collect();
+        self.add_token(TokenType::String, Some(Literal::Str(value)));
     }
 }
