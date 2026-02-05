55d627065c27e638be2d326e88b0f4ea9c9835a2
blackfrog638 <blackfrog638@gmail.com>
2026-02-05 23:55:57 +0800

    feat: add expr

---
+++ src/expr.rs
@@ -0,0 +1,47 @@
+use crate::token::Token;
+
+pub enum Expr {
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Grouping {
+        expression: Box<Expr>,
+    },
+    Literal {
+        value: Token,
+    },
+    Unary {
+        operator: Token,
+        right: Box<Expr>,
+    },
+}
+
+impl Expr {
+    pub fn to_string(&self) -> String {
+        match self {
+            Expr::Binary {
+                left,
+                operator,
+                right,
+            } => {
+                format!(
+                    "({} {} {})",
+                    operator.lexeme,
+                    left.to_string(),
+                    right.to_string()
+                )
+            }
+            Expr::Grouping { expression } => {
+                format!("(group {})", expression.to_string())
+            }
+            Expr::Literal { value } => {
+                value.to_string() // 使用我们在 Token 里实现的 Display
+            }
+            Expr::Unary { operator, right } => {
+                format!("({} {})", operator.lexeme, right.to_string())
+            }
+        }
+    }
+}
